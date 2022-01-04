b03840172709a302ca19b8add13af78ea6e98d97
DEEP MEHTA <deep.mm@somaiya.edu>
2022-01-04 06:24:16 +0000

    Update script

--- .github/scripts/createLanguageProject.ps1
+++ .github/scripts/createLanguageProject.ps1
@@ -15,12 +15,12 @@ param(
     $subscriptionKey = "09fdb01632734be2bf0c7754b1d52ac7"
 )
 
-$body = @{"defaultAnswer" = "Sorry, I cannot help you with that right now"
-    "language"   = "en"
+$body = @{"defaultAnswer"  = "Sorry, I cannot help you with that right now"
+    "language"             = "en"
     "multilingualResource" = $false
-    "name" = $projectName
-    "projectName" = $projectName
-    "settings" = @{
+    "name"                 = $projectName
+    "projectName"          = $projectName
+    "settings"             = @{
         "defaultAnswer" = "Sorry, I cannot help you with that right now"
     }
 }
@@ -29,13 +29,14 @@ Invoke-RestMethod -Uri "https://$($QnAName)-language.cognitiveservices.azure.com
 
 Start-Sleep -s 60
 
-#"files"      = @( @{
-#    "fileName"       = "qna_chitchat_Friendly.tsv"
-#    "fileUri"        = "https://qnamakerstore.blob.core.windows.net/qnamakerdata/editorial/english/qna_chitchat_friendly.tsv"
-#    "isUnstructured" = $false
-#} )
+$body = @(@{
+        "op"    = "add"
+        "value" = @{
+            "displayName" = "qna_chitchat_witty"
+            "source"      = "qna_chitchat_Friendly.tsv"
+            "sourceUri"   = "https://qnamakerstore.blob.core.windows.net/qnamakerdata/editorial/english/qna_chitchat_witty.tsv"
+            "sourceKind"  = "file"
+        }
+    })
 
-#$knowledgeBases = (Invoke-RestMethod -Uri "$($QnAEndpoint)qnamaker/v4.0/knowledgebases" -Method GET -Headers @{"Ocp-Apim-Subscription-Key" = "$subscriptionKey"; "Content-Type" = "application/json" }).knowledgebases | Sort-Object -Property createdTimestamp -Descending
-#$knowledgeBaseId = $knowledgeBases[0].id
-#
-#Write-Output($knowledgeBaseId);
+Invoke-RestMethod -Uri "https://$($QnAName)-language.cognitiveservices.azure.com/language/query-knowledgebases/projects/$($projectName)/sources?api-version=2021-10-01" -Method PATCH -Headers @{"Ocp-Apim-Subscription-Key" = "$subscriptionKey"; "Content-Type" = "application/json" } -ContentType "application/json" -Body $($body | ConvertTo-Json)
