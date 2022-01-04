f8feb857a96e07aabca709c5d2d15197474f6da4
DEEP MEHTA <deep.mm@somaiya.edu>
2022-01-04 06:40:16 +0000

    Update language project

--- .github/scripts/createLanguageProject.ps1
+++ .github/scripts/createLanguageProject.ps1
@@ -2,17 +2,17 @@ param(
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
     [String]
-    $QnAName = "qna-maker-ip",
+    $QnAName,
 
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
     [String]
-    $projectName = "qna-maker-ip",
+    $projectName,
 
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
     [String]
-    $subscriptionKey = "09fdb01632734be2bf0c7754b1d52ac7"
+    $subscriptionKey
 )
 
 $body = @{"defaultAnswer"  = "Sorry, I cannot help you with that right now"
@@ -29,14 +29,18 @@ Invoke-RestMethod -Uri "https://$($QnAName)-language.cognitiveservices.azure.com
 
 Start-Sleep -s 60
 
-$body = @(@{
-        "op"    = "add"
-        "value" = @{
-            "displayName" = "qna_chitchat_witty"
-            "source"      = "qna_chitchat_Friendly.tsv"
-            "sourceUri"   = "https://qnamakerstore.blob.core.windows.net/qnamakerdata/editorial/english/qna_chitchat_witty.tsv"
-            "sourceKind"  = "file"
-        }
-    })
+$body = @{
+    "op"    = "add"
+    "value" = @{
+        "displayName" = "qna_chitchat_witty"
+        "source"      = "qna_chitchat_Friendly.tsv"
+        "sourceUri"   = "https://qnamakerstore.blob.core.windows.net/qnamakerdata/editorial/english/qna_chitchat_witty.tsv"
+        "sourceKind"  = "file"
+    }
+}
 
-Invoke-RestMethod -Uri "https://$($QnAName)-language.cognitiveservices.azure.com/language/query-knowledgebases/projects/$($projectName)/sources?api-version=2021-10-01" -Method PATCH -Headers @{"Ocp-Apim-Subscription-Key" = "$subscriptionKey"; "Content-Type" = "application/json" } -ContentType "application/json" -Body $($body | ConvertTo-Json)
+Invoke-RestMethod -Uri "https://$($QnAName)-language.cognitiveservices.azure.com/language/query-knowledgebases/projects/$($projectName)/sources?api-version=2021-10-01" -Method PATCH -Headers @{"Ocp-Apim-Subscription-Key" = "$subscriptionKey"; "Content-Type" = "application/json" } -ContentType "application/json" -Body "[$($body | ConvertTo-Json)]"
+
+Start-Sleep -s 10
+
+Invoke-RestMethod -Uri "https://$($QnAName)-language.cognitiveservices.azure.com/language/query-knowledgebases/projects/$($projectName)/deployments/production?api-version=2021-10-01" -Method PUT -Headers @{"Ocp-Apim-Subscription-Key" = "$subscriptionKey"; "Content-Type" = "application/json" }
